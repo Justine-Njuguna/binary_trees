460fc0a5266121fb565e6635a4210092bf45d169
Justine-Njuguna <hdenlysjustine@gmail.com>
2024-03-31 13:33:37 +0300

    Task-16

--- binary_trees.h
+++ binary_trees.h
@@ -59,4 +59,9 @@ size_t binary_tree_height(const binary_tree_t *tree);
 
 int binary_tree_is_full(const binary_tree_t *tree);
 
+size_t binary_tree_height(const binary_tree_t *tree);
+
+int binary_tree_is_perfect(const binary_tree_t *tree);
+
+
 #endif /* BINARY_TREES_H */
